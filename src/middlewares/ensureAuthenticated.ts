04cee3134f5055848fe906cb28bca9e36cd1b8f7
josed <jose.silva478@fatec.sp.gov.br>
2021-02-01 12:02:58 -0300

    middleware created

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 function ensureAuthentication(request: Request, response: Response, next: NextFunction): void{
+    const webToken = request.headers.authorization;
+
+    if (!webToken){
+        throw new Error('JWT does not exists');
+    }
+
+    const verifiedToken = verify(webToken, authConfig.webToken.secret);
+
+    
     
 }
