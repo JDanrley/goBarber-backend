60e45306830469118d4cb501f28e1f0f2a55ac6e
josed <jose.silva478@fatec.sp.gov.br>
2021-02-09 09:16:41 -0300

    jwt validation

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -2,15 +2,32 @@ import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '../config/auth';
 
-function ensureAuthentication(request: Request, response: Response, next: NextFunction): void{
-    const webToken = request.headers.authorization;
+interface TokenPayload {
+    iat: number;
+    expt: number;
+    sub: string;
+}
 
-    if (!webToken){
+function ensureAuthentication(request: Request, response: Response, next: NextFunction): void{
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader){
         throw new Error('JWT does not exists');
     }
 
-    const verifiedToken = verify(webToken, authConfig.webToken.secret);
+    const [, token] = authHeader.split(' ');
 
-    
-    
+    const { secret, expiresIn } = authConfig.webToken;
+
+    try {
+        const decoded = verify(token, secret);
+
+        console.log(decoded);
+
+        return next();
+    } catch {
+        throw new Error('JWT is not valid');
+    }   
 }
+
+export default ensureAuthentication;
